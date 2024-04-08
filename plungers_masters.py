caef63ff92fc239c79861bfd1eeb8dd6af3a5890
Greg Murray <gregthemurray@gmail.com>
2024-04-08 14:11:47 -0700

    added Similar Entries Tab

--- plungers_masters.py
+++ plungers_masters.py
@@ -80,11 +80,7 @@ df = get_data(url)
 df['row_num'] = df.reset_index().index
 df['Name'].str.strip()
 
-
-names = sorted(list(df['Name'].unique()))
-names.insert(0,'All Entries (Defualt)')
-name_sbx = st.selectbox('Select an Entrant', names, placeholder='All Entries (Defualt)',)
-
+# Data Prep
 
 #Create Entry Details 
 g1 = df[['Rank','Entry','Name','Golfer 1','R1','R2','R3','R4', 'Tot','row_num','Total']]
@@ -94,58 +90,119 @@ g4 = df[['Rank','Entry','Name','Golfer 4','R1.3','R2.3','R3.3','R4.3','Tot.3','r
 g5 = df[['Rank','Entry','Name','Golfer 5','R1.4','R2.4','R3.4','R4.4','Tot.4','row_num','Total']]
 g6 = df[['Rank','Entry','Name','Golfer 6','R1.5','R2.5','R3.5','R4.5','Tot.5','row_num','Total']]
 
+#clean up 
 entries_d = pd.DataFrame(np.vstack([g1,g2,g3,g4,g5,g6]), columns=g1.columns)
 entries_d.rename(columns={'Golfer 1':'Golfer'},inplace=True)
 entries_d.sort_values(by=['row_num','Tot',],inplace=True)
 
-#Check for golfer list similarity
-# entries_d.sort_values(by=['row_num','Golfer',],inplace=True)
-
-# entries_test = entries_d.groupby(['Rank','Entry','Total'])['Golfer'].apply(list).reset_index()
-# entries_string = entries_d.groupby(['Rank','Entry','Total'])['Golfer'].apply(','.join).reset_index()
-
-# count_list = entries_string.groupby(['Golfer'])['Total'].count().reset_index()
-# count_list
+# group Golfers in to lists per entry
+entry_golfer_list = entries_d.groupby(['Rank','Entry','Total','row_num'])['Golfer'].apply(list).reset_index()
 
 
-if name_sbx != 'All Entries (Defualt)':
-    entries = df.query("Name == '{}'".format(name_sbx))
-    details = entries_d.query("Name == '{}'".format(name_sbx))
+tab1, tab2 = st.tabs(['Leaderboard','Similar Entries'])
 
 
-    st.subheader('Entries')
-    entries = entries[['Rank', 'Entry','Total','T1','T2']]  
-    show_entries = entries[['Rank', 'Entry','Total',]]  
-    st.dataframe(show_entries,hide_index=True)
-   
-    st.subheader('Entry Details')
+with tab1:
+    names = sorted(list(df['Name'].unique()))
+    names.insert(0,'All Entries (Defualt)')
+    name_sbx = st.selectbox('Select an Entrant', names, placeholder='All Entries (Defualt)',)
 
-    # entry detail data prep
-    details = details.reset_index()
-    details = details.drop(columns=['index','row_num','Name'])
-
-    # display entries
-    for i in range(4):
-        format_entry(entries, details,i)
+    if name_sbx != 'All Entries (Defualt)':
+        entries = df.query("Name == '{}'".format(name_sbx))
+        details = entries_d.query("Name == '{}'".format(name_sbx))
 
 
-else:
-    st.subheader(':trophy: Standings')
-    df.drop(columns=['row_num'], inplace=True)
+        st.subheader('Entries')
+        entries = entries[['Rank', 'Entry','Total','T1','T2']]  
+        show_entries = entries[['Rank', 'Entry','Total',]]  
+        st.dataframe(show_entries,hide_index=True)
+    
+        st.subheader('Entry Details')
 
-    if det_rad == 'Scores': 
-        df_style = df[['Rank','Entry','Total']]
-    elif det_rad == 'Golfers':
-        df_style = df[['Rank','Entry','Total','Golfer 1','Tot',
-                       'Golfer 2','Tot.1','Golfer 3','Tot.2',
-                       'Golfer 4','Tot.3','Golfer 5','Tot.4',
-                       'Golfer 6','Tot.5','T1','T2']]
+        # entry detail data prep
+        details = details.reset_index()
+        details = details.drop(columns=['index','row_num','Name'])
+
+        # display entries
+        for i in range(4):
+            format_entry(entries, details,i)
+
+
     else:
-        df.drop(columns=['Name'], inplace=True)
-        df_style = df.style\
-            .background_gradient(cmap=cmap,subset=rds,vmin = rd_min,vmax=rd_max)\
-            .highlight_null('white')
+        st.subheader(':trophy: Standings')
+        df.drop(columns=['row_num'], inplace=True)
+
+        if det_rad == 'Scores': 
+            df_style = df[['Rank','Entry','Total']]
+        elif det_rad == 'Golfers':
+            df_style = df[['Rank','Entry','Total','Golfer 1','Tot',
+                        'Golfer 2','Tot.1','Golfer 3','Tot.2',
+                        'Golfer 4','Tot.3','Golfer 5','Tot.4',
+                        'Golfer 6','Tot.5','T1','T2']]
+        else:
+            df.drop(columns=['Name'], inplace=True)
+            df_style = df.style\
+                .background_gradient(cmap=cmap,subset=rds,vmin = rd_min,vmax=rd_max)\
+                .highlight_null('white')
+
+            
+            
+        st.dataframe(df_style, hide_index=True)
+
+with tab2:
+    entry_list = sorted(list(entry_golfer_list['Entry']))
+    sbx_entry = st.selectbox('Choose an Entry',entry_list, placeholder='Entry Name')
+    
+    if sbx_entry != 'Entry Name':
+        sel_entry = entry_golfer_list.query("Entry == '{}'".format(sbx_entry))
+        sim_df = pd.DataFrame()
+        for idx, row in entry_golfer_list.iterrows():
+
+            entry_list = set(sel_entry['Golfer'].iloc[0])
+            compare_list = set(row['Golfer'])
+            common = list(entry_list & compare_list)
+            different = compare_list - entry_list
+            row['count_sim'] = len(common)
+            row['common'] = sorted(common)
+            row['different'] = sorted(different)
+
+            row = pd.DataFrame(row).transpose()
+
+            sim_df = pd.concat([sim_df,row], ignore_index=True)
 
         
+        # st.dataframe(sel_entry['Golfer'],hide_index=True)
         
-    st.dataframe(df_style, hide_index=True)
+        golfer_str =''
+        for n, golfer in enumerate(sorted(sel_entry['Golfer'].iloc[0])):
+            if n == 5:
+                golfer_str += golfer
+            else:
+                golfer_str += golfer+ ', '
+        with st.container(border=True):
+            st.write('''Entry: **{}** | Rank: **{}** | Total: **{}**  
+                    Golfers:  
+                    {}'''.format(sel_entry['Entry'].iloc[0],
+                                        sel_entry['Rank'].iloc[0],
+                                        sel_entry['Total'].iloc[0],
+                                        golfer_str ))
+            
+        g_sim = sim_df.groupby(['count_sim'])['row_num'].count().reset_index()
+
+        g_sim['title'] = g_sim['count_sim'].astype(str) + ' Golfer(s)'
+        g_sim = pd.pivot_table(g_sim,values=['row_num'],columns=['title'])
+        g_sim = g_sim[g_sim.columns[::-1]]
+        g_sim = g_sim.style.background_gradient(axis=1)\
+                           .format(precision=0)
+        
+        
+        
+        
+        st.write('**Entry Counts by # of similar Golfers :golfer:**')
+        st.dataframe(g_sim,hide_index=True)
+        # st.bar_chart(g_sim,)
+
+        with st.expander('See Entry Similarity Details'):
+            sim_df.sort_values(['count_sim','row_num'],ascending =[False,True],inplace=True)
+            sim_display = sim_df[['Rank','Entry','Total','count_sim','common','different']]
+            st.dataframe(sim_display,hide_index=True)
